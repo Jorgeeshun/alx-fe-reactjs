3e2addaffdf0d6b86565535a31902f3da6dd92da
jorgeeshun <georgeprinceakeshun23@gmail.com>
2025-08-31 20:36:08 +0000

    Components Importation: App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
       <div>
         <UserProfile
           name = "Alice"
-          age = {25}
+          age = "25"
           bio = "Loves hiking and photography" 
         />
       </div>
