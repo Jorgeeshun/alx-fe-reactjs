2b136ba3e427786500b47e0bcfb60dfa1bc40372
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2025-09-07 21:22:19 +0000

    New repository Created: alx-react-app-props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import ProfilePage from "./components/ProfilePage";
+import ProfilePage from "./ProfilePage";
 import UserContext from "./UserContext";
 
 function App() {
-  const userData = { 
-    name: "George Prince Eshun", 
-    email: "georgeprinceakeshun23@gmail.com"
-  };
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-  <UserContext.Provider value={userData}>
-    <ProfilePage/>
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
     </UserContext.Provider>
   );
 }
