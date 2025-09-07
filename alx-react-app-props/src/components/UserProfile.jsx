e081b60e7bedee742188ff2aa94791f03a94e930
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2025-09-07 23:24:14 +0000

    alx-reat-app-props repository Updated

---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserInfo from "./UserInfo";
+
+function UserProfile() {
+  return <UserInfo />;  // no props, context will handle data
+}
+
+export default UserProfile;
