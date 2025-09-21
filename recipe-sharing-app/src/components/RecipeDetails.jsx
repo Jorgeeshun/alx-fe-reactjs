c150aaf3f83022ea98dc1ba23e7a870932358423
Jorgeeshun <georgeprinceakeshun23@gmail.com>
2025-09-21 22:09:03 +0000

    New react App: recipe sharing app

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { useRecipeStore } from '../store/recipeStore';
+import { useRecipeStore } from './recipeStore';
 import DeleteRecipeButton from './DeleteRecipeButton';
 
 
